5fdf58eba9ee23f9cc490270fc9fbeca88da23b5
quttylee <quttylee@gmail.com>
2025-07-23 16:39:41 +0900

    Add capacitor.config.ts
    Add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.babygender.calculator',
+  appName: '재미로 보는 아기성별계산기',
+  webDir: 'dist/public'
+};
+
+export default config;
